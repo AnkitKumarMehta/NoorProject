dddb69e99071a3fd98ccd7620953f227a8c2001f
Ankit Kumar <ankmehta33@gmail.com>
2021-10-15 14:40:50 +0530

    final changes

--- index.php
+++ index.php
@@ -13,13 +13,58 @@
     <link rel="stylesheet" href="assets/css/vendor/aos.css" />
     <link rel="stylesheet" href="assets/css/plugins/feature.css" />
     <!-- Style css -->
-    <link rel="stylesheet" href="assets/css/landingPage.css" />
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="assets/css/landingPage.css">
+    <link rel="stylesheet" href="assets/css/Articles.css">
 </head>
 
 <body>
     
-<?php require_once "header.php" ?>
+<!-- <?php require_once "header.php" ?> -->
+
+<div class="pos-rel">
+    <nav class="navbar navbar-expand-lg customNav">
+        <a class="navbar-brand" href="#">
+            <img src="./assets/images/donut-logo-02.png" alt="logo" class="homeLogo" />
+        </a>
+        <button
+            class="navbar-toggler toggleIcon w-auto"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+                    <span class="navbar-toggler-icon">
+                        <img
+                            src="https://img.icons8.com/fluency/48/000000/menu--v1.png"
+                            class="navbar-toggler-icon toggleIcon"
+                        />
+                    </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav ml-auto customNavGroup">
+                <li class="nav-item mx-3 customNavItems">
+                    <a class="nav-link nav-link-txt customNavFont clr-White" href="about.php" target="_blank">About</a>
+                </li>
+                <li class="nav-item mx-3 customNavItems">
+                    <a class="nav-link nav-link-txt customNavFont clr-White" href="articles.php" target="_blank">Articles</a>
+                </li>
+                <li class="nav-item mx-3 customNavItems">
+                    <a class="nav-link nav-link-txt customNavFont clr-White" href="podcast.php" target="_blank">Podcast</a>
+                </li>
+                <li class="nav-item mx-3 customNavItems">
+                    <a class="nav-link nav-link-txt customNavFont clr-White" href="freeResources.php" target="_blank">Free Resources</a>
+                </li>
+                <li class="nav-item mx-3 customNavItems">
+                    <a class="nav-link nav-link-txt customNavFont clr-White" href="about.php#contacts" target="_blank">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
 
 <section class="slider-style-5 with-particles">
     <div
