f01fb1b07b45d67e01c60bf740a08dfb56590c36
Ankit Kumar <ankmehta33@gmail.com>
2021-10-15 14:40:25 +0530

    header fix and reponsive design fix

--- articles.php
+++ articles.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/donut-logo-02.png">
     <title>Articles</title>
     <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/vendor/slick.css">
