cfb27f0564d4fd460919c61e20367cb4b5d0880c
shubham <singhshubham873@gmail.com>
2021-10-15 13:06:44 +0530

    podcast page header

---
+++ header.php
@@ -0,0 +1,42 @@
+<div class="pos-rel">
+    <nav class="navbar navbar-expand-lg customNav">
+        <a class="navbar-brand" href="#">
+            <img src="./assets/images/donut-logo-02.png" alt="logo" class="homeLogo" />
+        </a>
+        <button
+            class="navbar-toggler toggleIcon"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+                    <span class="navbar-toggler-icon">
+                        <img
+                            src="https://img.icons8.com/fluency/48/000000/menu--v1.png"
+                            class="navbar-toggler-icon toggleIcon"
+                        />
+                    </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav ml-auto customNavGroup">
+                <li class="nav-item">
+                    <a class="nav-link nav-link-txt customNavFont clr-Black" href="">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-txt customNavFont clr-Black">Articles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-txt customNavFont clr-Black">Podcast</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-txt customNavFont clr-Black">Free Resources</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-txt customNavFont clr-Black">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
